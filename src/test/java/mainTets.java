d8f36fe86f04e9a9fae0d319969f37b1223051c4
1030628998 <julian.yepes@segurosbolivar.com>
2023-04-27 14:02:15 -0500

    Inicio del proyecto

--- src/test/java/mainTets.java
+++ src/test/java/mainTets.java
@@ -1,8 +1,13 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class mainTets {
-    public static void main(String[] args) {
+    private static WebDriver driver;
+    @BeforeAll
+    public static void setUp() {
         // Establecer la propiedad del sistema para indicar la ubicación del driver de Chrome
         System.setProperty("webdriver.chrome.driver", "C://driver/chromedriver.exe");
 
@@ -11,10 +16,14 @@ public class mainTets {
 
         // Navegar a la página de Google
         driver.get("https://www.google.com");
-
+    }
+    @Test
+    public static void init() {
         // Imprimir el título de la página
         System.out.println("El título de la página es: " + driver.getTitle());
-
+    }
+    @AfterAll
+    public static  void tearDown(){
         // Cerrar el navegador
         driver.quit();
     }
